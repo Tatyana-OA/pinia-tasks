38a2ce3d90b858f5409bccd1c44a89066fcba06c
TAsenova <tasenova@mentormate.com>
2024-09-26 15:14:45 +0300

    install pinia, setup a basic store

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
-<script setup></script>
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
 
-<template></template>
+<script>
+export default {}
+</script>
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('taskStore', {
+  state: () => ({
+    tasks: [
+      { id: 1, title: 'Organize your workspace for maximum productivity', isFav: true },
+      { id: 2, title: 'Learn a new recipe and cook it for dinner', isFav: false },
+      { id: 3, title: 'Read a book outside your usual genre', isFav: true },
+      { id: 4, title: 'Complete a challenging puzzle', isFav: false },
+      { id: 5, title: 'Plan a weekend getaway to a new place', isFav: true }
+    ]
+  })
+})
